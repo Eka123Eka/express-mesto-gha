154a273060190f9252c5e64c9563b845d5245a81
Ekaterina Dolgopolova <elkod@yandex.ru>
2023-07-25 22:03:31 +0300

    Ревью 2 итерация 1 подключен validator, helmet, исправлены dependencies

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 const routes = require('./routes');
 
-const { PORT = 3000, SERVER_ADR = 'http://127.0.0.1' } = process.env;
+const { PORT = 3000, SERVER_ADR = 'http://127.0.0.1', DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+mongoose.connect(DB_URL, {
   useNewUrlParser: true,
 }).then(() => console.log(new Date(), 'connected to db:', mongoose.connections[0].name));
 
 const app = express();
 
+app.use(helmet());
 app.use(express.json());
 app.use((req, res, next) => {
   req.user = {
