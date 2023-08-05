97ea010edfd0f8c7aa4632135364094e3b8ccb9a
Ekaterina Dolgopolova <elkod@yandex.ru>
2023-08-05 19:56:00 +0300

    Ревью 0 итерация 4

--- controllers/users.js
+++ controllers/users.js
@@ -49,7 +49,12 @@ const createUser = (req, res, next) => {
     .then((hash) => User.create({
       name, about, avatar, email, password: hash,
     }))
-    .then((user) => res.send({ password, ...user }))
+    .then((user) => res.send({
+      name: user.name,
+      about: user.about,
+      avatar: user.avatar,
+      email: user.email,
+    }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         next(new BadRequestError(`При создании пользователя переданы некорректные данные.
