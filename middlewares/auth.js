f2fac6adc32ed4c0bb89a5ada810066f83d4e9aa
Ekaterina Dolgopolova <elkod@yandex.ru>
2023-08-05 19:49:15 +0300

    Ревью 0 итерация 3

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,13 +4,13 @@ const { AuthError } = require('../errors');
 const { JWT_SECRET_KEY } = require('../utils/constants');
 
 const auth = (req, res, next) => {
-  const { authData } = req.headers;
+  const { authorization } = req.headers;
 
-  if (!authData || !authData.startsWith('Bearer ')) {
+  if (!authorization || !authorization.startsWith('Bearer ')) {
     throw new AuthError('Для продолжения требуется авторизоваться');
   }
 
-  const token = authData.replace('Bearer ', '');
+  const token = authorization.replace('Bearer ', '');
 
   let payload;
 
--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,7 @@
 const {
   PORT = 3000,
   SERVER_ADR = 'http://127.0.0.1',
-  DB_URL = 'mongodb://localhost:27017/mestodb',
+  DB_URL = 'mongodb://127.0.0.1:27017/mestodb',
   JWT_SECRET_KEY = 'secret-key',
 } = process.env;
 const timeInMs = 10 * 60 * 1000;
